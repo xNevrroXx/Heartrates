c37e7c26814e7c6990029d970de9bd5278b3ede7
Vladimir <govorov2001gv@gmail.com>
2021-09-01 20:15:17 +0300

    clear project. Package.json works. Go dev!

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,25 @@ const browserSync = require('browser-sync');
 const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require('gulp-rename');
+const rename = require("gulp-rename");
 
-// Static server
 gulp.task('server', function() {
-    browserSync.init({
+
+    browserSync({
         server: {
             baseDir: "src"
-        }
+        }   
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
     return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-        }))
+        .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer({
-            browser: ['last 2 versions'],
+            overrideBrowserslist: ['last 2 versions'],
             cascade: false
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
@@ -32,7 +31,6 @@ gulp.task('styles', function(){
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
