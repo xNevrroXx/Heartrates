93dc1469849cef5e3e4d407be88539c06688091b
Vladimir <govorov2001gv@gmail.com>
2021-09-13 03:30:20 +0300

    add tabs and the same content for the test

--- src/js/script.js
+++ src/js/script.js
@@ -39,7 +39,7 @@ const slider = tns({
   responsive: {
     992: {
       mouseDrag: true,
-      autoHeight: true,
+      autoHeight: false,
       nav: false,
       touch: false
     }
@@ -52,4 +52,20 @@ document.querySelector('.carousel__prev').addEventListener('click',function () {
 
 document.querySelector('.carousel__next').addEventListener('click',function () {
   slider.goTo('next');
+});
+
+$(document).ready(function(){
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      
+      $( ".catalog__content_active#tabWithTwoItems").css("justify-content","flex-start" );
+      if( $(".catalog__content_active#tabWithTwoItems").css("justify-content") == "flex-start" ) {
+        $(".catalog-item").css("margin-right", "20px");
+      }
+      if( $(".catalog__content_active:not(#tabWithTwoItems)").css("justify-content") == "space-between" ) {
+        $(".catalog-item").css("margin-right", "0px");
+      }
+  });
 });
